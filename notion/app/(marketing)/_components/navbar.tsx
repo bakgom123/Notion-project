d7754345d0b9e633b91f8f4ed21661c843ef56d1
bakgom123 <gunjudavid@naver.com>
2024-02-29 02:15:08 -0600

    notion day1-3

---
+++ notion/app/(marketing)/_components/navbar.tsx
@@ -0,0 +1,58 @@
+"use client"
+
+import { useScrollTop } from "@/hooks/use-scroll-top"
+import { cn } from "@/lib/utils";
+import { Logo } from "./logo";
+import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
+
+
+
+
+export const Navbar = () => {
+    const { isAuthenticated, isLoading } = useConvexAuth();
+    const scrolled = useScrollTop();
+    // 스크롤 했을 때 경계를 보여주는 스타일
+    return (
+        <div className={cn(
+            "z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6",
+            scrolled && "border-b shadow-sm"
+        )}>
+            <Logo />
+            <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+                {isLoading && (
+                    <Spinner />
+                )}
+                {!isLoading && !isAuthenticated && (
+                    <>
+                        <SignInButton mode="modal">
+                            <Button variant="ghost" size="sm">
+                                Log in
+                            </Button>
+                        </SignInButton>
+                        <SignInButton mode="modal">
+                            <Button size="sm">
+                                Get Yootion free
+                            </Button>
+                        </SignInButton>
+                    </>
+                )}
+                {isAuthenticated && !isLoading && (
+                    <>
+                        <Button variant="ghost" size="sm" asChild>
+                            <Link href="/documents">
+                                Enter Yootion
+                            </Link>
+                        </Button>
+                        <UserButton afterSignOutUrl="/" />
+                    </>
+                )}
+                <ModeToggle />
+            </div>
+        </div>
+    )
+}
